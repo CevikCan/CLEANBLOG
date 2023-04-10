56df43071bd62b8b289e160e5ad627726e103fc7
CevikCan <caancevik@gmail.com>
2023-04-10 16:33:53 +0300

    mongoose ile veritabanı  bağlantısı oluşturuldu. veritabanındaki bilgiler anasayfaya aktarıldı.

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
+const Post = require("./models/Post");
+
 const app = express();
 
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://127.0.0.1:27017/cleanblog-post-db");
+
 app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 app.set("view engine", "ejs");
 
-app.get("/", (req,res) => {
-    res.render("index");
+app.get("/", async (req,res) => {
+    const posts = await Post.find({});
+    res.render("index", {
+        posts
+    });
 })
 app.get("/about", (req,res) => {
     res.render("about");
 })
 app.get("/add-post", (req,res) => {
     res.render("add_post");
+})
+
+app.post("/posts", async (req,res) => {
+    await Post.create(req.body);
+    res.redirect("/");
 })
 
 const port = 3000;
